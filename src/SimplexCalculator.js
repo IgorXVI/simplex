f3eb1a25653b2f6473e93480edd8f806e4613620
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2020-11-09 17:30:24 -0300

    criando SimplexCalculator

---
+++ src/SimplexCalculator.js
@@ -0,0 +1,209 @@
+module.exports = () => {
+    const lineOps = opsFun => ({ allVars, line, number }) => allVars.reduce((hash, oneVar) => {
+        hash[oneVar] = opsFun({ varValue: line[oneVar], number })
+        return hash
+    }, {})
+
+    const divideLine = lineOps(({ varValue, number }) => varValue / number)
+
+    const multiplyLine = lineOps(({ varValue, number }) => varValue * number)
+
+    const sumLines = ({ allVars, line1, line2 }) => allVars.reduce((hash, oneVar) => {
+        hash[oneVar] = line1[oneVar] + line2[oneVar]
+        return hash
+    }, {})
+
+    const extractAllFuns = ({ Z, restrictions }) => [Z, ...restrictions]
+
+    const extractAllVars = allFuns => Object.keys(
+        allFuns
+            .map(fun => Object.keys(fun))
+            .reduce((allKeys, keys) => allKeys.concat(keys), [])
+            .reduce((hash, key) => {
+                hash[key] = true
+                return hash
+            }, {})
+    )
+
+    const makeTable = ({ allVars, allFuns }) => allFuns
+        .map(
+            fun => allVars.reduce(
+                (hash, oneVar) => {
+                    hash[oneVar] = fun[oneVar] || 0
+                    return hash
+                },
+                {}
+            )
+        )
+
+    const findMinVar = ({ allVars, table }) => allVars.reduce(
+        (currVar, oneVar) =>
+            table[0][oneVar] < table[0][currVar] ?
+                oneVar
+                : currVar
+        ,
+        allVars[0]
+    )
+
+    const findMainLineObj = ({ table, minVar }) => table.reduce(
+        (currObj, line, lineIndex) => {
+            if (lineIndex === 0 || line[minVar] === 0) {
+                return currObj
+            }
+
+            const lineValue = line["B"] / line[minVar]
+
+            return lineValue < currObj.value ?
+                { index: lineIndex, value: lineValue }
+                : currObj
+        }
+        ,
+        { index: -1, value: Infinity }
+    )
+
+    const extractMainLine = ({
+        table,
+        mainLineObj,
+        minVar
+    }) => table[mainLineObj.index][minVar]
+
+    const makeNewMainLine = ({
+        allVars,
+        table,
+        mainLine,
+        mainLineObj
+    }) => divideLine({
+        allVars,
+        number: mainLine,
+        line: table[mainLineObj.index]
+    })
+
+    const processLine = ({
+        line,
+        minVar,
+        newMainLine,
+        allVars
+    }) => {
+        const pivoForThis = multiplyLine({
+            line: newMainLine,
+            number: line[minVar] * (-1),
+            allVars
+        })
+
+        const newLineValue = sumLines({
+            line1: pivoForThis,
+            line2: line,
+            allVars
+        })
+
+        return newLineValue
+    }
+
+    const makeNewLinesRetrictions = ({
+        table,
+        mainLineObj,
+        minVar,
+        newMainLine,
+        allVars
+    }) => table
+        .filter((_, index) => index !== mainLineObj.index)
+        .map(line => processLine({
+            line,
+            minVar,
+            newMainLine,
+            allVars
+        }))
+
+    const makeNewTable = ({
+        newLinesRetrictions,
+        newMainLine
+    }) => [...newLinesRetrictions, newMainLine]
+
+    const calcNextSimplexTable = ({
+        table,
+        allVars
+    }) => {
+        const minVar = findMinVar({
+            allVars,
+            table
+        })
+
+        const mainLineObj = findMainLineObj({
+            minVar,
+            table
+        })
+
+        const mainLine = extractMainLine({
+            mainLineObj,
+            minVar,
+            table
+        })
+
+        const newMainLine = makeNewMainLine({
+            allVars,
+            mainLine,
+            mainLineObj,
+            table
+        })
+
+        const newLinesRetrictions = makeNewLinesRetrictions({
+            mainLineObj,
+            minVar,
+            newMainLine,
+            table,
+            allVars
+        })
+
+        const newTable = makeNewTable({
+            newLinesRetrictions,
+            newMainLine
+        })
+
+        return newTable
+    }
+
+    const testTableFail = ({
+        table,
+        allVars = []
+    }) => {
+        const lineZ = table.find(line => line.Z === 1)
+
+        return allVars.some(oneVar => lineZ[oneVar] < 0)
+    }
+
+    const getResult = table => table.find(line => line.Z === 1).B
+
+    const calcSimplex = ({
+        Z,
+        restrictions
+    }) => {
+        const allFuns = extractAllFuns({
+            Z,
+            restrictions
+        })
+
+        const allVars = extractAllVars(allFuns)
+
+        let table = makeTable({
+            allFuns,
+            allVars
+        })
+
+        while (testTableFail({ table, allVars })) {
+            const newTable = calcNextSimplexTable({
+                allVars,
+                table
+            })
+
+            table = newTable
+        }
+
+        const result = getResult(table)
+
+        return result
+    }
+
+    return {
+        calcSimplex
+    }
+}
